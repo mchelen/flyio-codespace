c3a881f00eee92bc57a2928181cd2c256cb79837
Mike Chelen (he/him) <michael.chelen@gmail.com>
2024-01-25 01:41:43 +0000

    fix

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -2,6 +2,6 @@
 
 curl -L https://fly.io/install.sh | sh
 
-echo "FLYCTL_INSTALL='/home/codespace/.fly' >> ~/.profile
+echo "FLYCTL_INSTALL='/home/codespace/.fly'" >> ~/.profile
 
 echo "PATH='$FLYCTL_INSTALL/bin:$PATH'" >> ~/.profile
